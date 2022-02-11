9dfb01a80b002080abc96905d77f97745f6e510a
ramriddlez <ramankangz@outlook.com>
2022-02-11 17:47:57 -0500

    changes in file

--- index.js
+++ index.js
@@ -3,78 +3,80 @@ const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 const questions = [
-    {
-      // Title
-      type: 'input',
-      message: 'What is the title for your project?',
-      name: 'title',
-    },
-    {
-      // Description
-      type: 'input',
-      message: 'Add a description for your project:',
-      name: 'description',
-    },
-    {
-      // How to install
-      type: 'input',
-      message: 'Provide your installation instructions:',
-      name: 'install',
-    },
-    {
-      // Usage
-      type: 'input',
-      message: 'Provide instructions and examples for use',
-      name: 'usage',
-    },
-    {
-        // Contribution 
-        type: 'input',
-        message: 'Add your contribution guidelines:',
-        name: "contribution",
-    },
-    {
-        // Tests 
-        type: 'input',
-        message: 'Add your test instructions:',
-        name: "test",
-      },
-    {
-      // License
-      type: 'list',
-      message: 'Select your license:',
-      choices: ["Apache 2.0","MIT","GNU GPLv3", "The Perl License", "None"],
-      name: 'license',
-    },
-    {
-      // Asks for github profile 
-      type: 'input',
-      message: 'Enter Github username:',
-      name: "github",
-    },
-    {
-      // Asks for email address 
-      type: 'input',
-      message: 'Enter email address:',
-      name: "email",
-    },
-  ] 
+  {
+    // Title
+    type: 'input',
+    message: 'What is the title for your project?',
+    name: 'title',
+  },
+  {
+    // Description
+    type: 'input',
+    message: 'Add a description for your project:',
+    name: 'description',
+  },
+  {
+    // How to install
+    type: 'input',
+    message: 'Provide your installation instructions:',
+    name: 'install',
+  },
+  {
+    // Usage
+    type: 'input',
+    message: 'Provide instructions and examples for use',
+    name: 'usage',
+  },
+  {
+    // Contribution 
+    type: 'input',
+    message: 'Add your contribution guidelines:',
+    name: "contribution",
+  },
+  {
+    // Tests 
+    type: 'input',
+    message: 'Add your test instructions:',
+    name: "test",
+  },
+  {
+    // License
+    type: 'list',
+    message: 'Select your license:',
+    choices: ["Apache 2.0", "MIT", "GNU GPLv3", "The Perl License", "None"],
+    name: 'license',
+  },
+  {
+    // Asks for github profile 
+    type: 'input',
+    message: 'Enter Github username:',
+    name: "github",
+  },
+  {
+    // Asks for email address 
+    type: 'input',
+    message: 'Enter email address:',
+    name: "email",
+  },
+]
 
-    function writeToFile(generateMarkdown, data) {
-        fs.writeFile(generateMarkdown, data, (err) => {
-            if (err) {
-             return console.log(err);
-        }
-        console.log("Success! Your README.md file has been generated")
-    });
+function writeToFile(generateMarkdown, data) {
+  fs.writeFile(generateMarkdown, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success! Your README.md file has been generated")
+  });
 }
 
 function init() {
-    inquirer.prompt(questions)
-    .then ((data) => {
-        const response = generateMarkdown(data.questions);
-        writeToFile("Generated_README.md",response);
-        console.log("a README.md has been created successfully!!");
+  inquirer.prompt(questions)
+    .then((data) => {
+      console.log(data)
+      const response = generateMarkdown(data);
+      
+      writeToFile("Generated_README.md", response);
+      console.log("a README.md has been created successfully!!");
     })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -80,7 +80,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 
-function renderLicenseSection(license) {
+function renderLicenseLink(license) {
   if (license === "Apache 2.0") {
     return`(https://opensource.org/licenses/Apache-2.0)`
   } else if (license === "MIT"){
@@ -97,8 +97,8 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   //const license = data.license;
   let licenseBadge = renderLicenseBadge(data.license);
-  let licenselink = renderLicenseLink(license);
-  let licensesection = renderLicenseSection(license);
+  let licenselink = renderLicenseLink(data.license);
+  let licensesection = renderLicenseSection(data.license);
   
   return `# ${data.title} ${licenseBadge}${licenselink}
 
